05d9eb70d12296c97f02edb10f7d2b1675656409
Kor-smg569 <smg56928@gmail.com>
2024-01-10 00:05:29 +0900

    ui working 1/10

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Appbar',
+      debugShowCheckedModeBanner: false,
+      title: 'G-VISION',
       theme: ThemeData(
-          primarySwatch: Colors.red
-      ),
+          useMaterial3: false,
+          colorScheme: ColorScheme.fromSeed(seedColor: Color(0xff4CAF50))),
       home: MyPage(),
     );
   }
@@ -25,20 +26,50 @@ class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
-        title: Text('Appbar test'),
-        centerTitle: true,
+        title: const Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'G-VISION',
+          ),
+        ),
         elevation: 0.0,
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {
-            print('menu button is clicked');
-            print('Git Test');
-          },
+        actions: [
+          GestureDetector(
+            onTap: () {
+              print("right bT");
+            },
+            child: const Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Icon(
+                Icons.add,
+                size: 40.0,
+                color: Colors.black,
+              ),
+            ),
+          )
+        ],
+      ),
+      body: Container(
+        color: const Color(0xffD3D3D3),
+        child: const Center(
+          child: Text("프로젝트 목록"),
         ),
       ),
 
+
+
+
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.view_list),
+              label: '프로젝트 목록'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.unarchive),
+              label: '공유함'),
+        ],
+      ),
     );
   }
 }
